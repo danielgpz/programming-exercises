30af49183432c41f4d1e8865af73960fde24e764
Daniel García <d.garciabfd@gmail.com>
2022-04-29 00:00:05 -0400

    Ejercicio de la CP 1
    Diferenciar el uso de virtual, override y new.

---
+++ cp1/ex1/Exercise1/Example/Example.cs
@@ -0,0 +1,61 @@
+﻿namespace Example
+{
+    /* Jerarquia de clases
+     *           A
+     *          / \
+     *         B   C
+     *         |
+     *         D
+     *         |
+     *         E <- new
+     *         |
+     *         F
+     *         |
+     *         G
+     */
+
+    public class A
+    {
+        public virtual void test() {
+            Console.WriteLine("Invocando metodo test desde la clase A");
+        }
+    }
+
+    public class B : A {
+        public override void test() { 
+            Console.WriteLine("Invocando metodo test desde la clase B");
+        }
+    }
+
+    public class C : A { }
+
+    public class D : B {
+        public override void test() {
+            Console.WriteLine("Invocando metodo test desde la clase D");
+        }
+    }
+
+    public class E : D {
+        public new void test() {
+            Console.WriteLine("Invocando metodo test desde la clase E");
+        }
+    }
+
+    public class F : E {
+        //public override void test()
+        //{
+        //    Console.WriteLine("Invocando metodo test desde la clase D");
+        //}
+        public new virtual void test()
+        {
+            Console.WriteLine("Invocando metodo test desde la clase F");
+        }
+    }
+
+    public class G : F {
+        public override void test()
+        {
+            Console.WriteLine("Invocando metodo test desde la clase G");
+        }
+    }
+}
---
+++ cp1/ex1/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Example;
+
+Console.WriteLine("\nPrueba: Invocando el metodo test por cada tipo");
+new A().test();
+new B().test();
+new C().test();
+new D().test();
+new E().test();
+new F().test();
+new G().test();
+
+Console.WriteLine("\nPrueba: un array de tipo A[], con una instancia de cada tipo");
+A[] arreglo = new A[] { new A(), new B(), new C(), new D(), new E(), new F(), new G()};
+foreach (var x in arreglo) {
+    x.test();
+}
+
+Console.WriteLine("\nPrueba: Invocando el metodo test por cada tipo casteando todo a tipo A");
+((A)new A()).test();
+((A)new B()).test();
+((A)new C()).test();
+((A)new D()).test();
+((A)new E()).test();
+((A)new F()).test();
+((A)new G()).test();
+
+Console.WriteLine("\nPrueba: un array de tipo A[], con una instancia de cada tipo a partir de E");
+A[] arreglo2 = new E[] { new E(), new F(), new G() };
+foreach (var x in arreglo2){
+    x.test();
+}
+
+Console.WriteLine("\nPrueba: un array de tipo E[], con una instancia de cada tipo a partir de E");
+E[] arreglo3 = new E[] { new E(), new F(), new G() };
+foreach (var x in arreglo3)
+{
+    x.test();
+}
+
+Console.WriteLine("\nPrueba: Miscelanea 1");
+A var1 = new D();
+var1.test();
+((B)var1).test();
+
+Console.WriteLine("\nPrueba: Miscelanea 2");
+A var2 = new F();
+var2.test();
+((F)var2).test();
+
+Console.WriteLine("\nPrueba: Miscelanea 3");
+A var3 = new G();
+var3.test();
+((F)var3).test();
+
