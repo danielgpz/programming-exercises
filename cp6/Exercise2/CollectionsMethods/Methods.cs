7112a35a8b152bbd8c95c8d74dfa9d4eaf496f5d
Daniel García <d.garciabfd@gmail.com>
2022-06-09 21:29:37 -0400

    Hide the solutions

--- cp6/Exercise2/CollectionsMethods/Methods.cs
+++ cp6/Exercise2/CollectionsMethods/Methods.cs
@@ -20,73 +20,46 @@ namespace CollectionsMethods
         // Tomar los primeros count elementos de un enumerable
         public static IEnumerable<T> Take<T>(IEnumerable<T> enumerable, int count) 
         {
-            int i = 0;
-            foreach (T elem in enumerable) 
-            {
-                if (i++ >= count)
-                    break;
-                yield return elem;
-            }
+            // Borre debajo y escriba su código
+            throw new NotImplementedException();
         }
 
         // Tomar solo los elementos que pasan el filtro filter
         public static IEnumerable<T> Filter<T>(IEnumerable<T> enumerable, Func<T, bool> filter)
         {
-            foreach (T elem in enumerable) 
-            {
-                if (filter.Invoke(elem))
-                    yield return elem;
-            }
+            // Borre debajo y escriba su código
+            throw new NotImplementedException();
         }
 
-        // Transformar los elementos del enumerable usando la funcion map
+        // Transformar los elementos del enumerable usando la función map
         public static IEnumerable<T2> Map<T1, T2>(IEnumerable<T1> enumerable, Func<T1, T2> map)
         {
-            foreach (T1 elem in enumerable)
-                yield return map(elem);
+            // Borre debajo y escriba su código
+            throw new NotImplementedException();
         }
 
         // Tomar un solo elemento por cada subsecuencia maximal de elementos
         // iguales
         public static IEnumerable<T> Distinct<T>(IEnumerable<T> enumerable, Func<T, T, bool> comparer)
         {
-            IEnumerator<T> aux = enumerable.GetEnumerator();
-            if (aux.MoveNext()) 
-            {
-                T last = aux.Current;
-                yield return last;
-                foreach (T elem in enumerable)
-                {
-                    if (!comparer(elem, last))
-                        yield return elem;
-                    last = elem;
-                }
-            }
+            // Borre debajo y escriba su código
+            throw new NotImplementedException();
         }
 
-        // Concatenar los enumerables contenidos en un enumerable coleccion en
-        // un unico enumerable
+        // Concatenar los enumerables contenidos en un enumerable colección en
+        // un único enumerable
         public static IEnumerable<T> Concat<T>(IEnumerable<IEnumerable<T>> collection)
         {
-            foreach (IEnumerable<T> enumerable in collection)
-            {
-                foreach (T elem in enumerable)
-                {
-                    yield return elem;
-                }
-            }
+            // Borre debajo y escriba su código
+            throw new NotImplementedException();
         }
 
         // Repetir cada elemento del enumerable la cantidad de veces que indique
-        // la funcion repeat para dicho elemento
+        // la función repeat para dicho elemento
         public static IEnumerable<T> Repeat<T>(IEnumerable<T> enumerable, Func<T, int> repeat)
         {
-            foreach (T elem in enumerable)
-            {
-                int times = repeat(elem);
-                while (times-- > 0)
-                    yield return elem;
-            }
+            // Borre debajo y escriba su código
+            throw new NotImplementedException();
         }
     }
 }
