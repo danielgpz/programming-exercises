c351fea13167540670302c67f25e263bf69fae43
Daniel García <d.garciabfd@gmail.com>
2022-06-09 21:26:29 -0400

    Add ex2 of cp6

---
+++ cp6/Exercise2/CollectionsMethods/Methods.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionsMethods
+{
+    public static class Methods
+    {
+        // Imprime un enumerable en la consola
+        public static void Print<T>(IEnumerable<T> enumerable) 
+        {
+            foreach (T elem in enumerable) 
+            {
+                Console.Write(elem);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+        // Tomar los primeros count elementos de un enumerable
+        public static IEnumerable<T> Take<T>(IEnumerable<T> enumerable, int count) 
+        {
+            int i = 0;
+            foreach (T elem in enumerable) 
+            {
+                if (i++ >= count)
+                    break;
+                yield return elem;
+            }
+        }
+
+        // Tomar solo los elementos que pasan el filtro filter
+        public static IEnumerable<T> Filter<T>(IEnumerable<T> enumerable, Func<T, bool> filter)
+        {
+            foreach (T elem in enumerable) 
+            {
+                if (filter.Invoke(elem))
+                    yield return elem;
+            }
+        }
+
+        // Transformar los elementos del enumerable usando la funcion map
+        public static IEnumerable<T2> Map<T1, T2>(IEnumerable<T1> enumerable, Func<T1, T2> map)
+        {
+            foreach (T1 elem in enumerable)
+                yield return map(elem);
+        }
+
+        // Tomar un solo elemento por cada subsecuencia maximal de elementos
+        // iguales
+        public static IEnumerable<T> Distinct<T>(IEnumerable<T> enumerable, Func<T, T, bool> comparer)
+        {
+            IEnumerator<T> aux = enumerable.GetEnumerator();
+            if (aux.MoveNext()) 
+            {
+                T last = aux.Current;
+                yield return last;
+                foreach (T elem in enumerable)
+                {
+                    if (!comparer(elem, last))
+                        yield return elem;
+                    last = elem;
+                }
+            }
+        }
+
+        // Concatenar los enumerables contenidos en un enumerable coleccion en
+        // un unico enumerable
+        public static IEnumerable<T> Concat<T>(IEnumerable<IEnumerable<T>> collection)
+        {
+            foreach (IEnumerable<T> enumerable in collection)
+            {
+                foreach (T elem in enumerable)
+                {
+                    yield return elem;
+                }
+            }
+        }
+
+        // Repetir cada elemento del enumerable la cantidad de veces que indique
+        // la funcion repeat para dicho elemento
+        public static IEnumerable<T> Repeat<T>(IEnumerable<T> enumerable, Func<T, int> repeat)
+        {
+            foreach (T elem in enumerable)
+            {
+                int times = repeat(elem);
+                while (times-- > 0)
+                    yield return elem;
+            }
+        }
+    }
+}
---
+++ cp6/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using CollectionsMethods;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Test1
+            Console.WriteLine("Test #1");
+            int[] arr1 = new int[] { 1, 2, 3, 0, 4, -9, 11, 129, -78, 34 };
+            IEnumerable<int> take = Methods.Take(arr1, 6);
+            Methods.Print(take);
+            Console.WriteLine();
+            #endregion
+            // Salida: 1 2 3 0 4 -9
+
+            #region Test2
+            Console.WriteLine("Test #2");
+            int[] arr2 = new int[] { 11, 20, -3, 101, 46, 88899, 113, 19, -42 };
+            IEnumerable<int> filter = Methods.Filter(arr2, x => x%2 == 0);
+            Methods.Print(filter);
+            Console.WriteLine();
+            #endregion
+            // Salida: 20 46 -42
+
+            #region Test3
+            Console.WriteLine("Test #3");
+            int[] arr3 = new int[] { 21, 72, 3, -161, -603, 11189, 13, -9234, 512 };
+            IEnumerable<double> map = Methods.Map(arr3, x => (x/2.0));
+            Methods.Print(map);
+            Console.WriteLine();
+            #endregion
+            // Salida: 10.5 36 1.5 -80.5 -301.5 5594.5 6.5 -4617 256
+
+            #region Test4
+            Console.WriteLine("Test #4");
+            int[] arr4 = new int[] { 1, 1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 4, 5, 5, 5 };
+            IEnumerable<int> distinct = Methods.Distinct(arr4, (x, y) => x == y);
+            Methods.Print(distinct);
+            Console.WriteLine();
+            #endregion
+            // Salida: 1 2 3 4 5
+
+            #region Test5
+            Console.WriteLine("Test #5");
+            IEnumerable<int>[] arr5 = new IEnumerable<int>[] { take, filter, distinct };
+            IEnumerable<int> concat = Methods.Concat(arr5);
+            Methods.Print(concat);
+            Console.WriteLine();
+            #endregion
+            // Salida: 1 2 3 0 4 -9 20 46 -42 1 2 3 4 5
+
+            #region Test6
+            Console.WriteLine("Test #6");
+            string[] arr6 = new string[] { "a", "bc", "def" };
+            IEnumerable<string> repeat = Methods.Repeat(arr6, s => s.Length);
+            Methods.Print(repeat);
+            Console.WriteLine();
+            #endregion
+            // Salida: a bc bc def def def
+        }
+    }
+}
